bf6ffaa396385c2c35fb8df9e6af154ef0a531ea
Tolulope Olanrewaju <lanre.toluwa@gmail.com>
2023-11-07 23:42:08 +0100

    feat: replace deprecated styles with modern version pydantic settings config

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from dotenv import load_dotenv
-from pydantic import Field
+from pydantic import Field, ConfigDict
 from pydantic_settings import BaseSettings
 
 
@@ -8,19 +8,17 @@ BASE_DIR = Path(__file__).resolve().parent
 load_dotenv()
 
 class Settings(BaseSettings):
+    model_config = ConfigDict(frozen=True)
+
     UPLOAD_DIR: Path = BASE_DIR / "uploads"
     VECTORSTORE_DIR: Path = BASE_DIR / "vectordb"
-    REDIS_URL: str = Field(default="redis://redis:6379", env="REDIS_URL")
-    OPENAI_API_KEY: str = Field(env="OPEN_API_KEY")
+    REDIS_URL: str = Field(default="redis://redis:6379", json_schema_extra={"env": "REDIS_URL"})
+    OPENAI_API_KEY: str = Field(json_schema_extra={"env": "OPEN_API_KEY"})
     SUPPORTED_FILE_TYPES: tuple[str] = ("pdf",)
 
 
     def model_post_init(self, *args) -> None:
         self.UPLOAD_DIR.mkdir(parents=True, exist_ok=True)
         self.VECTORSTORE_DIR.mkdir(parents=True, exist_ok=True)
-
-    class Config:
-        frozen = True
-
 
 settings = Settings()
