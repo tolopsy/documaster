b4d7f56b279283057939be46513637fa502878d2
Tolulope Olanrewaju <lanre.toluwa@gmail.com>
2023-11-07 23:40:10 +0100

    clean: remove db dependent utility function

--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,4 @@
-from schemas.document import Document
-from database import DB
 from config import settings
-    
-
-async def fetch_documents_to_inquire(document_ids: list[str], db: DB) -> list[Document]:
-    if document_ids:
-        docs = await db.fetch_documents_by_ids(ids=document_ids)
-    else:
-        docs = await db.fetch_all_documents()
-    
-    return docs
 
 def is_supported_file_type(filename: str | None) -> str | None:
     extension = filename.rsplit(".", maxsplit=1).pop() if "." in filename else None
