f66b2f6675b5f2ef283dee5211f16835e32b79ca
Tolulope Olanrewaju <lanre.toluwa@gmail.com>
2023-11-08 18:10:08 +0100

    feat: tests for upload endpoint

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -1,8 +1,63 @@
-def test_post_upload(mock_db, mock_storage):
+
+from pathlib import Path
+from unittest.mock import patch, AsyncMock
+from fastapi.testclient import TestClient
+
+from .testutils import is_4xx, is_2xx
+
+TEST_DATA_DIR = Path(__file__).resolve().parent / "data"
+TEST_BAD_UPLOAD_DATA = TEST_DATA_DIR / "test_bad_upload.png"
+TEST_GOOD_UPLOAD_DATA = TEST_DATA_DIR / "test_good_upload.pdf"
+
+
+def test_post_upload_returns_4xx_for_unsupported_file_types(client: TestClient):
+    with open(TEST_BAD_UPLOAD_DATA, "rb") as f:
+        response = client.post("/upload", files={"files": f})
+
+    assert is_4xx(response.status_code)
+
+def test_post_upload_unsupported_file_not_stored(client: TestClient):
+    with patch("storage.FileSystemStorage.store") as mock_storage:
+        with open(TEST_BAD_UPLOAD_DATA, "rb") as f:
+            _ = client.post("/upload", files={"files": f})
+        
+        mock_storage.assert_not_called()
+
+def test_post_upload_unsupported_file_not_saved_to_db(client: TestClient):
+    with patch("database.RedisDB.get_or_create") as mock_db:
+        with open(TEST_BAD_UPLOAD_DATA, "rb") as f:
+            _ = client.post("/upload", files={"files": f})
+        
+        mock_db.assert_not_called()
+
+def test_post_upload_returns_2xx_for_supported_file_types(client: TestClient, mock_storage, mock_db):
+    with open(TEST_GOOD_UPLOAD_DATA, "rb") as f:
+        response = client.post("/upload", files={"files": f})
+
+    assert is_2xx(response.status_code)
+
+def test_post_upload_stores_supported_file_in_storage(
+        client: TestClient,
+        mock_storage: AsyncMock,
+        mock_db: AsyncMock):
+
+    with open(TEST_GOOD_UPLOAD_DATA, "rb") as f:
+        _ = client.post("/upload", files={"files": f})
+
+    mock_storage.store.assert_called_once()
+
+def test_post_upload_saves_document_in_db(
+        client: TestClient,
+        mock_storage: AsyncMock,
+        mock_db: AsyncMock):
+
+    with open(TEST_GOOD_UPLOAD_DATA, "rb") as f:
+        _ = client.post("/upload", files={"files": f})
+
+    mock_db.save_document.assert_called_once()
+
+def test_list_document(client: TestClient, mock_db: AsyncMock):
     ...
 
-def test_get_documents(mock_db):
-    ...
-
-def test_post_ask(mock_db, mock_storage):
+def test_ask(client: TestClient, mock_db: AsyncMock, mock_storage: AsyncMock):
     ...
